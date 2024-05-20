b147557b2b9c45ac91288c64ad8b2b8768e60928
leon93br <113701577+leon93br@users.noreply.github.com>
2024-05-20 18:29:58 -0300

    Calcular Área de um triangulo
    Exercícios JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// Calcular Área de um triangulo
+
+const base = prompt('Digite a base do triângulo:');
+
+const altura = prompt('Digite a altura do triângulo:');
+
+const area = (base * altura) /2;
+
+alert(`A área do triângulo é : ${area}`);
